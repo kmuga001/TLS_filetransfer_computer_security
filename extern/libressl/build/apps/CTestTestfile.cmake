dbe4c0843a68b1d9ecfff894f22b7f6c204a39f6
Kinjal Mugatwala <kmuga001@ucr.edu>
2020-06-02 22:53:03 -0700

    trying to upload build

--- extern/libressl/build/apps/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/kmuga001/CS_165PROJECT/extern/libressl/apps
-# Build directory: /home/csmajs/kmuga001/CS_165PROJECT/extern/libressl/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("ocspcheck")
-subdirs("openssl")
-subdirs("nc")
--- extern/libressl/build/apps/ocspcheck/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/csmajs/kmuga001/CS_165PROJECT/extern/libressl/apps/ocspcheck
-# Build directory: /home/csmajs/kmuga001/CS_165PROJECT/extern/libressl/build/apps/ocspcheck
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
